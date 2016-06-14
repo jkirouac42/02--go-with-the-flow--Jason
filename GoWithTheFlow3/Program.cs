38d5fd89b59d5b055ec57e0d8964c8f02cb408d2
jkirouac42 <jkirouac42@gmail.com>
2016-06-14 16:17:22 -0400

    first commit

---
+++ GoWithTheFlow3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoWithTheFlow3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Should I go golfing today?");
+            Console.WriteLine("Is it raining?");
+
+            string userInput = Console.ReadLine();
+
+
+            if (userInput == "no")
+            {
+                Console.WriteLine("Stay home and play WII golf!");
+            }
+
+            else if (userInput == "yes")
+            {
+                Console.WriteLine("Stay home and play WII golf! It's cheaper!");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
